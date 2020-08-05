bc97c9f21034f8b6ca8b148070a6eec9ee22ba10
Georgi Todorov <georgi.vladimirov.todorov@gmail.com>
2020-08-05 17:28:01 +0300

    specify-scss-components

--- src/components/kendoButton/KendoButton.js
+++ src/components/kendoButton/KendoButton.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Button } from '@progress/kendo-react-buttons';
 
 function KendoButton() {
-  return <Button primary={true}>My Button with KendoReact styles</Button>;
+  return (
+    <Button icon="folder" primary={true} look="flat">
+      My Button with KendoReact styles
+    </Button>
+  );
 }
 
 export default KendoButton;
